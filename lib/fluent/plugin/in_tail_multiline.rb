b6e9d8cff625538984e427b45d845dec79c2cba9
Tomohisa Ota <tomohisa.ota+github@gmail.com>
2013-03-02 19:13:04 +0900

    Create base files to start extending tail plugin
    - Override tail plugin method with exactly same implementation.
    - Create unit test

--- lib/fluent/plugin/in_tail_multiline.rb
+++ lib/fluent/plugin/in_tail_multiline.rb
@@ -11,5 +11,27 @@ module Fluent
       super
     end
     
+    def receive_lines(lines)
+      es = MultiEventStream.new
+      lines.each {|line|
+        begin
+          line.chomp!  # remove \n
+          time, record = parse_line(line)
+          if time && record
+            es.add(time, record)
+          end
+        rescue
+          $log.warn line.dump, :error=>$!.to_s
+          $log.debug_backtrace
+        end
+      }
+      unless es.empty?
+        begin
+          Engine.emit_stream(@tag, es)
+        rescue
+          # ignore errors. Engine shows logs and backtraces.
+        end
+      end
+    end
   end
 end
--- test/plugin/test_in_tail_multiline.rb
+++ test/plugin/test_in_tail_multiline.rb
@@ -1,11 +1,51 @@
-require "fluent/plugin/tail/multiline/version"
+require 'helper'
 
-module Fluent
-  module Plugin
-    module Tail
-      module Multiline
-        # Your code goes here...
-      end
+require 'tempfile'
+
+class TailMultilineInputTest < Test::Unit::TestCase
+  def setup
+    Fluent::Test.setup
+  end
+
+  CONFIG = %[
+  ]
+  # CONFIG = %[
+  #   path #{TMP_DIR}/out_file_test
+  #   compress gz
+  #   utc
+  # ]
+
+  def create_driver(conf = CONFIG)
+    Fluent::Test::InputTestDriver.new(Fluent::TailMultilineInput).configure(conf)
+  end
+  
+  def test_emit
+    tmpFile = Tempfile.new("in_tail_multiline-")
+    puts tmpFile.path
+    File.open(tmpFile.path, "w") {|f|
+      f.puts "test1"
+      f.puts "test2"
+    }
+
+    d = create_driver %[
+      path #{tmpFile.path}
+      tag test
+      format /(?<message>.*)/
+    ]
+
+    d.run do
+      sleep 1
+
+      File.open(tmpFile.path, "a") {|f|
+        f.puts "test3"
+        f.puts "test4"
+      }
+      sleep 1
     end
+
+    emits = d.emits
+    assert_equal(true, emits.length > 0)
+    assert_equal({"message"=>"test3"}, emits[0][2])
+    assert_equal({"message"=>"test4"}, emits[1][2])   
   end
 end
