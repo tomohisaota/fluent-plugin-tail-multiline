72b7ed50bf140581012025f20b07048ec3435272
Tomohisa Ota <tomohisa.ota+github@gmail.com>
2013-03-03 16:52:15 +0900

    Flush log buffer after 1 sec

--- lib/fluent/plugin/in_tail_multiline.rb
+++ lib/fluent/plugin/in_tail_multiline.rb
@@ -6,11 +6,13 @@ module Fluent
     config_param :format, :string
     config_param :format_firstline, :string, :default => nil
     config_param :rawdata_key, :string, :default => nil
+    config_param :auto_flush_sec, :integer, :default => 1
     
     def initialize
       super
-      @locker = Mutex::new
+      @locker = Monitor.new
       @logbuf = nil
+      @logbuf_flusher = CallLater::new
     end
     
     def configure(conf)
@@ -19,6 +21,7 @@ module Fluent
     end
     
     def receive_lines(lines)
+      @logbuf_flusher.cancel()
       es = MultiEventStream.new
       @locker.synchronize do
         lines.each {|line|
@@ -38,17 +41,22 @@ module Fluent
           end
         }
       end
-      return if es.empty?
-      begin
-        Engine.emit_stream(@tag, es)
-      rescue
-        # ignore errors. Engine shows logs and backtraces.
+      unless es.empty?
+        begin
+          Engine.emit_stream(@tag, es)
+        rescue
+          # ignore errors. Engine shows logs and backtraces.
+        end
+      end
+      @logbuf_flusher.call_later(@auto_flush_sec) do
+        flush_logbuf()
       end
     end
     
     def shutdown
       super
       flush_logbuf()
+      @logbuf_flusher.shutdown()
     end   
 
     def flush_logbuf
@@ -73,4 +81,69 @@ module Fluent
     
   end
   
+  class CallLater 
+    def initialize
+      @locker = Monitor::new
+      @thread = Thread.new(&method(:run))
+      initExecBlock()
+    end
+    
+    def call_later(delay,&block)
+      @locker.synchronize do
+        @exec_time = Engine.now + delay
+        @exec_block = block
+      end
+      @thread.run
+    end
+    
+    def run
+      @running = true
+      while true
+        sleepSec = -1
+        @locker.synchronize do
+          now = Engine.now
+          if @exec_block && @exec_time <= now
+            @exec_block.call()
+            initExecBlock()
+          end          
+          return unless @running
+          unless(@exec_time == -1)
+            sleepSec = @exec_time - now 
+          end
+        end
+        if (sleepSec == -1)
+          sleep()
+        else
+          sleep(sleepSec)
+        end
+      end
+    rescue => e
+      puts e
+    end
+    
+    def cancel()
+      initExecBlock()
+    end
+
+    def shutdown()
+      @locker.synchronize do
+        @running = false
+      end
+      if(@thread)
+        @thread.run
+        @thread.join
+        @thread = nil
+      end
+    end    
+    
+    private 
+
+    def initExecBlock()
+      @locker.synchronize do
+        @exec_time = -1
+        @exec_block = nil
+      end
+    end
+    
+  end
 end
